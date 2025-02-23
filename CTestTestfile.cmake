8b943f09eb135d87b53c687eb2d564e9eb1cdfef
KholoodS <sjkholood@gmail.com>
2025-02-23 14:59:36 +0530

    Initial commit with project files

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/kholo/RealTimeAnalyticsPlatform
+# Build directory: C:/Users/kholo/RealTimeAnalyticsPlatform/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(tests "C:/Users/kholo/RealTimeAnalyticsPlatform/build/tests.exe")
+set_tests_properties(tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/kholo/RealTimeAnalyticsPlatform/CMakeLists.txt;28;add_test;C:/Users/kholo/RealTimeAnalyticsPlatform/CMakeLists.txt;0;")
